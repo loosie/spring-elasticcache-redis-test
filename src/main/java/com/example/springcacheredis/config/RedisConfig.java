6e59f9a2deb92992f335a4651496a36aa37cebcd
jongwon <54282927+loosie@users.noreply.github.com>
2022-04-20 18:33:15 +0900

    feat: redis config host, port 주입

--- src/main/java/com/example/springcacheredis/config/RedisConfig.java
+++ src/main/java/com/example/springcacheredis/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.springcacheredis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +17,15 @@ import com.example.springcacheredis.dto.ItemDto;
 @EnableCaching
 public class RedisConfig {
 
+	@Value("${spring.redis.host}")
+	private String host;
+
+	@Value("${spring.redis.port}")
+	private int port;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory(){
-		return new LettuceConnectionFactory();
+		return new LettuceConnectionFactory(host, port);
 	}
 
 	@Bean
